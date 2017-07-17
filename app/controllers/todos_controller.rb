7a4745225ea11637cbc4e8a744e61463e02dc91b
William Murphy <willmurphy100@gmail.com>
2017-07-17 06:28:11 +0000

    Update feature added

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -18,6 +18,20 @@ class TodosController < ApplicationController
     @todo = Todo.find(params[:id])
   end
   
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+  
+  def update
+    @todo = Todo.find(params[:id])
+    if Todo.update(todo_params)
+      flash[:notice] = "Todo has been succesfully updated"
+      redirect_to todo_path(@todo)
+    else
+      render 'edit'
+    end
+  end
+  
   private
   
     def todo_params
